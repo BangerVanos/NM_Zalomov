3d6b03f61756272eff638288e37b12eb83eec421
Raman Zalomau <romchik349@gmail.com>
2022-09-26 09:30:47 +0300

    Done python for IPT1

--- IPT1/Python/tailor.py
+++ IPT1/Python/tailor.py
@@ -1,7 +1,7 @@
 import math
 
 
-E = 10E-15
+E = 10E-10
 
 
 def tailor_term(n: int, x: float) -> float:
@@ -10,11 +10,22 @@ def tailor_term(n: int, x: float) -> float:
 
 def sin_tailor(term_num: int, x: float):
     tailor_list = [tailor_term(i, x) for i in range(term_num)]
-    print(', '.join(list(map(str, tailor_list))))
+    print("Tailor terms", ', '.join(list(map(str, tailor_list))))
     sin = sum(tailor_list)
     return sin
 
 
-print("-"*(len("Computed sin\tTailor sin")+3))
+def scores() -> str:
+    return "-"*(len("Computed sin\tTailor sin")+3)
+
+
+print(scores())
 print("Computed sin\tTailor sin")
-print("-"*(len("Computed sin\tTailor sin")+3))
+for x in range(26):
+    print(scores())
+    print(f"Counting sin({x})")
+    n = 1
+    while abs(math.sin(x) - sin_tailor(n, x)) >= E:
+        print(f"Computed sin({x}): {math.sin(x)}\tTailor sin({x}): {sin_tailor(n, x)}")
+        n += 1
+    print(f"{n} tailor terms needed to count sin with {E} accuracy")
