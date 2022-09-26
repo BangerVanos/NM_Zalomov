53b561c38213da2a3f10c296c888a1e0ba159718
Roman <romchik349@gmail.com>
2022-09-26 11:47:14 +0300

    ...

--- IPT1/Python/tailor.py
+++ IPT1/Python/tailor.py
@@ -1,7 +1,8 @@
 import math
 
 
-E = 10E-10
+E = 1E-5
+PI = math.pi
 
 
 def tailor_term(n: int, x: float) -> float:
@@ -9,23 +10,32 @@ def tailor_term(n: int, x: float) -> float:
 
 
 def sin_tailor(term_num: int, x: float):
+    # while x >= 2*PI:
+    #     x -= 2*PI
     tailor_list = [tailor_term(i, x) for i in range(term_num)]
-    print("Tailor terms", ', '.join(list(map(str, tailor_list))))
+    # print("Tailor terms:", ', '.join(list(map(str, tailor_list))))
     sin = sum(tailor_list)
     return sin
 
 
 def scores() -> str:
-    return "-"*(len("Computed sin\tTailor sin")+3)
+    return "-"*200
 
 
 print(scores())
-print("Computed sin\tTailor sin")
-for x in range(26):
+print("Computed sin\t\tTailor sin")
+for x in range(30):
     print(scores())
-    print(f"Counting sin({x})")
+    print(f"Computing sin({x})")
     n = 1
     while abs(math.sin(x) - sin_tailor(n, x)) >= E:
-        print(f"Computed sin({x}): {math.sin(x)}\tTailor sin({x}): {sin_tailor(n, x)}")
+        print(f"{n}. Computed sin({x}): {math.sin(x)}\t\tTailor sin({x}): {sin_tailor(n, x)}\t\t"
+              f"E={abs((math.sin(x)-sin_tailor(n, x))/sin_tailor(n, x))}")
         n += 1
+    try:
+        print(f"{n}. Computed sin({x}): {math.sin(x)}\t\tTailor sin({x}): {sin_tailor(n, x)}\t\t"
+              f"E={abs((math.sin(x) - sin_tailor(n, x)) / sin_tailor(n, x))}")
+    except ZeroDivisionError:
+        print(f"{n}. Computed sin({x}): {math.sin(x)}\t\tTailor sin({x}): {sin_tailor(n, x)}\t\t"
+              f"E=0")
     print(f"{n} tailor terms needed to count sin with {E} accuracy")
