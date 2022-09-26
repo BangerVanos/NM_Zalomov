32312dd9faf90f7f4d60a9f90136d2aeeb862d17
Raman Zalomau <romchik349@gmail.com>
2022-09-26 09:17:26 +0300

    Written tailor func

--- IPT1/Python/tailor.py
+++ IPT1/Python/tailor.py
@@ -1,3 +1,20 @@
 import math
 
-print(round(math.sin(15), 6))
+
+E = 10E-15
+
+
+def tailor_term(n: int, x: float) -> float:
+    return (-1)**n*((x**(2*n+1))/math.factorial(2*n+1))
+
+
+def sin_tailor(term_num: int, x: float):
+    tailor_list = [tailor_term(i, x) for i in range(term_num)]
+    print(', '.join(list(map(str, tailor_list))))
+    sin = sum(tailor_list)
+    return sin
+
+
+print("-"*(len("Computed sin\tTailor sin")+3))
+print("Computed sin\tTailor sin")
+print("-"*(len("Computed sin\tTailor sin")+3))
